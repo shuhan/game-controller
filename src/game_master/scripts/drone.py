e3b84b9e7a333464d38a547642befec68f58638e
Shah-nawaj Shuhan <s.shuhan@gmail.com>
2019-06-30 15:07:39 +0100

    Updated Drone Module

--- src/game_master/scripts/drone.py
+++ src/game_master/scripts/drone.py
@@ -10,26 +10,53 @@ from geometry_msgs.msg import Twist
 class Controller:
     '''
     Impliments controller for the bebop drone
+
+    ** Spin and rate shall be implimented by user module
+    ** Controller.process() shall be called on every tick to process pending messages
     '''
 
     CAMERA_PT_SPEED = 0.5
 
-    def __init__(self, movement_speed = 0.3, turning_speed = 0.5):
+    def __init__(self, movement_speed=0.3, turning_speed=0.5, annonymous=False):
         self.movement_speed = movement_speed
         self.turning_speed  = turning_speed
         self.status         = -1
         self.takeoff_pub    = rospy.Publisher('/bebop/takeoff', Empty, queue_size=1)
         self.land_pub       = rospy.Publisher('/bebop/land', Empty, queue_size=1)
         self.navi_pub       = rospy.Publisher('/bebop/cmd_vel', Twist, queue_size=1)
-        self.rate           = rospy.Rate(100)
-        rospy.init_node('drone__controller')
+        rospy.init_node('drone__controller', anonymous=annonymous)
 
-    def setSpeed(self, movement_speed = 0.3, turning_speed = 0.5):
+    def setSpeed(self, movement_speed=0.3, turning_speed=0.5):
         '''
         Set the speed components of the drone
         '''
         self.movement_speed = movement_speed
         self.turning_speed  = turning_speed
+
+    def onStatusChanged(self, data):
+        '''
+        Callback function for drone status change
+        '''
+
+    def onBatteryLevelChanged(self, data):
+        '''
+        Callback function for battery level change
+        '''
+
+    def onWifiSignalChnaged(self, data):
+        '''
+        Callback function for wifi signal status change
+        '''
+
+    def onPositionChanged(self, data):
+        '''
+        Callback function for drone position changed
+        '''
+
+    def onAltitudeChanged(self, data):
+        '''
+        Callback function for altitude changed
+        '''
 
     def takeoffLanding(self):
         '''
@@ -95,5 +122,10 @@ class Controller:
     def cameraTiltUp(self):
         '''
         Tilt the camera upward
+        '''
+
+    def process(self):
+        '''
+        Send all pending messages and spin once
         '''
         
