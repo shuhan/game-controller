c8da5de93a6e6e44206fd82fdcbdb3ee7c685cbf
Shah-nawaj Shuhan <ss2665@york.ac.uk>
2019-08-04 14:19:34 +0100

    Working with target tracker

---
+++ src/game_master/scripts/target_tracker.py
@@ -0,0 +1,59 @@
+
+from drone import BebopDrone
+
+class Goal:
+
+    def __init__(self, drone):
+        self.drone              = drone
+        #
+        self.enableHeight       = False
+        self.holdHeight         = False
+        self.heightTarget       = 0
+        self.onHeight           = False
+        #
+        self.enableOrientation  = False
+        self.holdOrientation    = False
+        self.orientationTarget  = 0
+        self.onOrientation      = False
+        #
+        self.enableDistance     = False
+        self.holdDistance       = False
+        self.distanceTarget     = 0
+        self.onDistance         = False
+
+    def setHeightTarget(self, targetHeight, hold=True):
+        self.enableHeight       = True
+        self.holdHeight         = hold
+        self.onHeight           = False
+        self.heightTarget       = targetHeight
+
+    def unsetHeightTarget(self):
+        self.enableHeight       = False
+        self.holdHeight         = False
+        self.heightTarget       = 0
+        self.onHeight           = False
+        
+    def setOrientationTarget(self, targetOrientation, hold=True):
+        self.enableOrientation  = True
+        self.holdOrientation    = hold
+        self.onOrientation      = False
+        self.orientationTarget  = targetOrientation
+
+    def unsetOrientationTarget(self):
+        self.enableOrientation  = False
+        self.holdOrientation    = False
+        self.orientationTarget  = 0
+        self.onOrientation      = False
+
+    def setDistanceTarget(self, targetDistance, hold=True):
+        self.enableDistance     = True
+        self.holdDistance       = hold
+        self.onDistance         = False
+        self.distanceTarget     = targetDistance
+
+    def unsetDistanceTarget(self):
+        self.enableDistance     = False
+        self.holdDistance       = False
+        self.distanceTarget     = 0
+        self.onDistance         = False
+
